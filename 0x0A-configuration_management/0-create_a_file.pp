2fd89e79b43f75d6a576a7dac3541abc6b84f2ea
Lilieyen <lnyasita@gmail.com>
2021-04-06 21:16:47 +0300

    craete a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file in /tmp using puppet
 
-file { [ '/tmp/holberton' ]:
+file { 'holberton':
     ensure  => directory,
     owner   => 'www-data',
     group   => 'www-data',
     mode    => '0744',
     content => 'I love Puppet',
-    path    => /tmp/holberton,
+    path    => '/tmp/holberton',
 }
