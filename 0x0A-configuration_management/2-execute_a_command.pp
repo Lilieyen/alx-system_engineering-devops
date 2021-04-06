07260406522ad44e02b048f7b372ab95a2bdcf9e
Lilieyen <lnyasita@gmail.com>
2021-04-06 21:44:33 +0300

    kill a process named killmenow using exec and pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 #creating a manifest that kills a process named killmenow using exec and pkill
 
 exec { 'pkill':
-ensure  => 'stopped',
-command => 'pkill -f killmenow',
+ensure   => 'stopped',
+command  => 'pkill -f killmenow',
+provider => 'shell'
 }
