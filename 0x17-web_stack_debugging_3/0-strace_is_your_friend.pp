653ccdc6a5b01b5954df7d4c334aedbcba5c03d4
Lilieyen <lnyasita@gmail.com>
2021-05-19 22:43:23 +0300

    fix Apache 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # fix issue of Apache returning a 500 error
 
-exec { 'fix typo error':
-  ensure  => 'test -e /var/www/html/wp-settings.php',
+exec { 'fix misprint error':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  path    => '/usr/local/bin/:/bin/'
 }
