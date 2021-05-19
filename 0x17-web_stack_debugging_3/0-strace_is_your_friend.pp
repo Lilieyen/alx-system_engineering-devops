435c3173090f066fddd54fa581d72a37c98372f5
Lilieyen <lnyasita@gmail.com>
2021-05-19 22:45:25 +0300

    fix Apache 500 error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix misprint error':
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
